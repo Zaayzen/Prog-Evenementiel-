9c3f00fb523d63c34513a7b072807b3e1cae4a58
Zaayzen <zamoum.aimen@gmail.com>
2026-01-20 17:43:03 +0100

    Exo1

--- Morpion.java
+++ Morpion.java
@@ -4,14 +4,24 @@ import javax.swing.*;
 public class Morpion {
     public static void main(String[] args) {
         int cellSize = 100;
-        var f = new JFrame("Morpion");
+        var f = new JFrame("Morpion J1");
+        var f1 = new JFrame("Morpion J2");
         f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         f.setLocationRelativeTo(null);
-        
+
+        f1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        f1.setLocationRelativeTo(null);
+
         var p = new JPanel(null);
+        var p1 = new JPanel(null);
         p.setPreferredSize(new Dimension(cellSize*3,cellSize*3));
+        p1.setPreferredSize(new Dimension(cellSize*3,cellSize*3));
+
         f.setContentPane(p);
         f.pack();
+
+        f1.setContentPane(p1);
+        f1.pack();
 
         var b = new JButton[3][3];
         var j = new int[] { 1 };
@@ -27,13 +37,26 @@ public class Morpion {
                         j[0] = (j[0] == 1) ? 2 : 1;
                     }
                     // Vérifier si gagnant ou fin de partie avec égalité.
+                    boolean egalite = true;
+                    for (int i = 0; i < 3; i++){
+                        for (int k = 0 ; k < 3; k++){
+                            if(b[i][k].getText().equals("-")){
+                                egalite = false;
+                            }
+                        }
+                    }
+                    if(egalite){
+                        System.exit(0);
+                    }
                 });
                 p.add(b[l][c]);
             }
         }
         f.pack();
         
+        f1.pack();
         f.setVisible(true);
+        f1.setVisible(true);
         System.out.println("end of main");
     }
     
